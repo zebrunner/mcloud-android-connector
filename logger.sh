3978afe2f7a7e609d658da7008020cdc93e5dbff
azarouski <azarouski@solvd.com>
2024-04-16 15:32:35 +0200

    logger comment was edited

--- logger.sh
+++ logger.sh
@@ -10,8 +10,8 @@
 # Examples:
 # > logger info info_message
 #   [26/03/2024 10:20:22] [INFO] info_message
-# > logger ErroR error_messsage
-#   [26/03/2024 10:22:53] [ERROR] error_messsage
+# > logger Error error_message
+#   [26/03/2024 10:22:53] [ERROR] error_message
 # > logger no_logger_level
 #   [26/03/2024 10:24:38] [INFO] no_logger_level
 # NOTE: if logger level is not set it will always be 'INFO'
